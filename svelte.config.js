7d5fc518a19bb718f54a596a832e8cc46c90055e
strawberria <akitocodes@gmail.com>
2022-07-05 14:14:13 -0700

    Static test

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static"; 
 import preprocess from 'svelte-preprocess';
 
 const dev = "production" === "development";
@@ -10,7 +10,14 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: "docs",
+            assets: "docs"
+		}),
+		paths: {
+            // change below to your repo name
+            base: "",
+        },
 
 		// Override http methods in the Todo forms
 		methodOverride: {
